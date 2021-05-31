589c0a5a65ce2ec5553fec75759207a9745d37bb
Alex Vázquez <alex.vazquez@unity3d.com>
2021-05-31 10:01:48 +0200

    Fix include

--- com.unity.render-pipelines.core/Runtime/Volume/VolumeComponent.cs
+++ com.unity.render-pipelines.core/Runtime/Volume/VolumeComponent.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Reflection;
 using System.Linq;
-using Codice.CM.SEIDInfo;
 
 namespace UnityEngine.Rendering
 {
